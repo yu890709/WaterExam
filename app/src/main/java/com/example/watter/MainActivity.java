9e29e1176fe5ed6b9a4a6065974ff3d0542a9aba
miia <miia@com.tw>
2019-12-31 14:02:04 +0800

    yu first commit

--- app/src/main/java/com/example/watter/MainActivity.java
+++ app/src/main/java/com/example/watter/MainActivity.java
@@ -36,7 +36,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    int n;
     private static final String TAG = MainActivity.class.getSimpleName();
     private EditText edMonthly;
     boolean isNext = false;
